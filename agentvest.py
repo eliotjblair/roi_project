5bb3f298ddb24f079662d0ebe8eaaf6baa41b776
eliotjblair <eliotjblair@gmail.com>
2025-06-03 20:29:19 -0400

    Added scrolling feature

--- agentvest.py
+++ agentvest.py
@@ -3,11 +3,14 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
 
 class ROIForm(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(orientation='vertical', spacing=10, padding=20, **kwargs)
+        self.size_hint_y = None
+        self.bind(minimum_height=self.setter('height'))
         self.inputs = {}
 
         fields = [
@@ -25,16 +28,17 @@ class ROIForm(BoxLayout):
         ]
 
         for label_text, key in fields:
-            self.add_widget(Label(text=label_text))
-            input_box = TextInput(multiline=False, input_filter='float')
+            label = Label(text=label_text, size_hint_y=None, height=30)
+            input_box = TextInput(multiline=False, input_filter='float', size_hint_y=None, height=40)
             self.inputs[key] = input_box
+            self.add_widget(label)
             self.add_widget(input_box)
 
-        calc_btn = Button(text="Calculate ROI", size_hint=(1, 0.3))
+        calc_btn = Button(text="Calculate ROI", size_hint_y=None, height=50)
         calc_btn.bind(on_press=self.calculate_roi)
         self.add_widget(calc_btn)
 
-        self.result_label = Label(text="")
+        self.result_label = Label(text="", size_hint_y=None, height=120)
         self.add_widget(self.result_label)
 
     def calculate_roi(self, instance):
@@ -53,14 +57,12 @@ class ROIForm(BoxLayout):
             vacancy_percent = float(self.inputs["vacancy_percent"].text) / 100
 
             loan_amount = purchase_price - down_payment
-
             mortgage_payment = loan_amount * (interest_rate * (1 + interest_rate) ** loan_term) / ((1 + interest_rate) ** loan_term - 1)
 
             management_fees = monthly_rent * management_percent
             vacancy_loss = monthly_rent * vacancy_percent
 
             total_monthly_expenses = mortgage_payment + taxes + insurance + hoa + maintenance + management_fees + vacancy_loss
-
             monthly_cash_flow = monthly_rent - total_monthly_expenses
             annual_cash_flow = monthly_cash_flow * 12
             cash_on_cash_roi = (annual_cash_flow / down_payment) * 100
@@ -76,7 +78,11 @@ class ROIForm(BoxLayout):
 class AgentVestApp(App):
     def build(self):
         Window.title = "AgentVest"
-        return ROIForm()
+        scroll = ScrollView()
+        form = ROIForm()
+        scroll.add_widget(form)
+        return scroll
 
 if __name__ == "__main__":
     AgentVestApp().run()
+
